6c81321574c1dafc1a0c35b630320c384a540479
Aysha Williams <awilliams@mavenlink.com>
2019-01-04 17:45:02 -0700

    added a Container widget and set its background to blue

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+
+  MyApp({this.title});
+
+  final Widget title;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Hello, world!",
-        textDirection: TextDirection.ltr,
-      )
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+        child: Center(
+            child: Text(
+              "Hello, world!",
+              textDirection: TextDirection.ltr,
+            )
+        )
     );
   }
 }
