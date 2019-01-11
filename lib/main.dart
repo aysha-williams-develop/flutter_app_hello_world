651eb67ed906fdb7043a24f172bfb0f50488225c
Aysha Williams <awilliams@mavenlink.com>
2019-01-11 15:51:51 -0700

    added a bug icon to the app bar menu

--- lib/main.dart
+++ lib/main.dart
@@ -23,23 +23,14 @@ class MyApp extends StatelessWidget {
             tooltip: 'Search',
             onPressed: null,
           ),
+          IconButton(
+            icon: Icon(Icons.adb),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
         ],
       ),
 
-    );
-
-
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.amber[500]),
-        child: Center(
-            child: Text(
-              "Hello, world!",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.black87, fontSize: 50),
-            )
-        )
     );
   }
 }
